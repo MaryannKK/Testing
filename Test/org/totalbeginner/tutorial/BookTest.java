1686261b76b87316243bd9b3a2f1a7d9c9fabb02
Maryann K <maryann.krupa@hotmail.com>
2017-02-02 14:21:12 -0700

    Testing EGit Guide

---
+++ Test/org/totalbeginner/tutorial/BookTest.java
@@ -0,0 +1,16 @@
+package org.totalbeginner.tutorial;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BookTest {
+
+	@Test
+	public void testBook() {
+//		Book b1 = new Book("Great Expectations");
+//		assert(b1.title = "Great Expectations");
+//		assert("Great Expectations", b1.title);
+	}
+
+}
